1c6a408a116cd8b2f7bd81d9a704f356ca5fb022
dlowder-salesforce <dlowder@salesforce.com>
2017-08-12 00:59:51 -0700

    Apple TV zoom helper methods

--- src/components/containers/zoom-helpers.js
+++ src/components/containers/zoom-helpers.js
@@ -161,7 +161,27 @@ const Helpers = {
     return defaults({}, childrenDomain, originalDomain, domain);
   },
 
-  onMouseDown(evt) {
+
+  onMouseDown(evt, targetProps) {
+    evt.preventDefault();
+    const { isTVSelectable } = targetProps;
+    const { x, y } = Selection.getSVGEventCoordinates(evt);
+    if (isTVSelectable ) {
+      return [{
+        target: "parent",
+        mutation: () => {
+          return {
+            startX: x, startY: y, panning: false, scaling: false,
+            parentControlledProps: ["domain"]
+          };
+        }
+      }];
+    } else {
+      return _onMouseDown(evt, targetProps);
+    }
+  },
+
+  _onMouseDown(evt, targetProps) {
     evt.preventDefault();
     const { x, y } = Selection.getSVGEventCoordinates(evt);
     return [{
@@ -175,11 +195,11 @@ const Helpers = {
     }];
   },
 
-  onMouseUp() {
+  onMouseUp(evt, targetProps) {
     return [{
       target: "parent",
       mutation: () => {
-        return { panning: false };
+        return { panning: false, scaling: false };
       }
     }];
   },
@@ -188,12 +208,41 @@ const Helpers = {
     return [{
       target: "parent",
       mutation: () => {
-        return { panning: false };
+        return { panning: false, scaling: false };
       }
     }];
   },
 
   onMouseMove(evt, targetProps, eventKey, ctx, keepStart) { // eslint-disable-line max-params
+    const { isTVSelectable, scaling, panning, startX, startY } = targetProps;
+    const { x, y } = Selection.getSVGEventCoordinates(evt);
+    if (isTVSelectable) {
+      if (panning) {
+        return this._onMouseMove(evt, targetProps, eventKey, ctx, true);
+      } else if (scaling) {
+        evt.deltaY = y - startY;
+        return this.onWheel(evt, targetProps, eventKey, ctx);
+      } else {
+        const absDeltaX = Math.abs(x - startX);
+        const absDeltaY = Math.abs(y - startY);
+        const p = absDeltaX > absDeltaY;
+        const s = !p;
+        return [{
+          target: "parent",
+          mutation: () => {
+            return {
+              panning: p,
+              scaling: s
+            };
+          }
+        }];
+      }
+    } else {
+      _onMouseMove(evt, targetProps, eventKey, ctx);
+    }
+  },
+
+  _onMouseMove(evt, targetProps, eventKey, ctx, keepStart) { // eslint-disable-line max-params
     if (targetProps.panning) {
       const { scale, startX, startY, onDomainChange, dimension, zoomDomain } = targetProps;
       const { x, y } = Selection.getSVGEventCoordinates(evt);
